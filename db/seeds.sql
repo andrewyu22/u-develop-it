76156736f73f9ae7d8447e6c4360049b5d794d0f
Andrew Yu <andrewyu209@gmail.com>
2021-12-19 12:41:08 -0500

    Updated database table

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,3 +10,10 @@ VALUES
   ('Montague', 'Summers', 1),
   ('Octavia', 'Butler', 1),
   ('Unica', 'Zurn', 1);
+
+UPDATE candidates
+SET industry_connected = 1
+WHERE id = 3;
+
+DELETE FROM candidates
+WHERE first_name = "Montague";
